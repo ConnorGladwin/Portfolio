cf0c3a55311742b315715e7bd55bed1e35c648c0
ConnorGladwin <Cygladwin795@gmail.com>
2021-04-26 13:22:37 +0200

    Added hidden project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      'backgroundImage': "url('images/white-waves.png')"
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
